76daf4c7289e3aa6ea126ca463753cd89f4327aa
jear-her <jearwebdeveloper@gmail.com>
2026-06-05 11:55:40 +0700

    push

--- clients/src/index.php
+++ clients/src/index.php
@@ -11,7 +11,7 @@ $userName = $isLoggedIn ? $_SESSION['user_fname'] . ' ' . $_SESSION['user_lname'
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Book Shop – Bestseller Books</title>
+    <title>E-Book Shop </title>
     <meta name="description" content="Discover and buy the world's bestselling books online. Rich Dad Poor Dad, Mindset, Think and Grow Rich and more.">
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
